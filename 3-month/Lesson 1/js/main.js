3a4b81daf344e72a864f6a7c3d906502022b6c48
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-05 20:27:00 +0500

    var, alert, prompt

---
+++ 3-month/Lesson 1/js/main.js
@@ -0,0 +1,49 @@
+//? var
+// alert("Hello world!");
+// var x = 5;
+// x=9;
+// var y = 4;
+// var z = y-x;
+// var username = "Ibrohim";
+// console.log(5);
+
+//? Data types
+//* 1. Primitive vs Non-Primitive or Reference
+//* Primitive => 7
+//* String
+// var username = "Abror 1";
+// var username2 = 'Abror 2 ';
+// var username3 = `Abror 3`;
+// console.log(username, username2, username3);
+// var student = 'o\'quvchi';
+// var student = "o'quvchi";
+// var sentence = "Jasur: \"Men musobaqada yutdim\" dedi"
+// var sentence = 'Jasur: "Men musobaqada yutdim" dedi';
+// var username = "Javlon ";
+// var sentence = `${username}: "Men musobaqada o'qtuvchimni yutdim" deb yolg'on gapirdi`;
+// var sentence = '${username}: "Men musobaqada o\'qtuvchimni yutdim" deb yolg\'on gapirdi';
+// var sentence = username + ": \"Men musobaqada o'qtuvchimni yutdim\" deb yolg'on gapirdi";
+// console.log(sentence);
+//* Number
+// var x = 20;
+//* Math oparators => +, -, *, /
+//* % => qoldiq natija qaytarib beradi
+// console.log(x % 8);
+//* ** => 2**3
+// console.log(2**3);
+//* Boolean
+//* Null
+//* Undefined
+//* BigInt
+//* Symbol
+
+//* Non-Primitive or Reference
+//* Object
+// console.log("Salom developer!")
+
+//? alert vs prompt
+// alert("Javlon")
+// var username = prompt("Ismingiz nima?");
+// console.log(username);
+
+
