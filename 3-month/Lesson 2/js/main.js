2d5ceb094a1fb504c17f5c743a6f244c84ba228e
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-07 19:54:03 +0500

    object

--- 3-month/Lesson 2/js/main.js
+++ 3-month/Lesson 2/js/main.js
@@ -15,9 +15,20 @@
 //* Symbol("Hello") = Symbol("Hello")
 
 //? Object
-var user = {
-  name: "Usmon",
-  age: 22,
-  isMerried: false,
-};
-console.log(user);
+// var user = {
+//   name: "Usmon",
+//   age: 22,
+//   isMerried: false,
+//   education: {
+//     region: "Tashkent",
+//     school: 324,
+//     year: "2009 - 2022",
+//   },
+// };
+
+// var x = "education";
+
+// console.log(user.name, user.age);
+// console.log(user.education.region);
+// console.log(user.x);
+// console.log(user[x]);
