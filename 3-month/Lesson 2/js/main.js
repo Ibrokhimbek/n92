d91bf33ccf5dbca4b1007c6df11f663c9e127494
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-07 19:28:00 +0500

    boolean, null, undefined

---
+++ 3-month/Lesson 2/js/main.js
@@ -0,0 +1,12 @@
+//? Boolean, null, undefined, BigInt, Symbol
+//* true / false
+// var isMerried = false;
+// isMerried = true;
+// console.log(isMerried);
+//* null
+// var username = null;
+// username = prompt("Enter your username:");
+// console.log(username);
+//* undefined
+// var x;
+// console.log(x);
