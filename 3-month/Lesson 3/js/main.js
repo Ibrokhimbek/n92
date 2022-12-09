32094a9cd7d6accb6df00e502dfddfe89b0da6cb
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-09 19:39:33 +0500

    let vs const, confirm

---
+++ 3-month/Lesson 3/js/main.js
@@ -0,0 +1,29 @@
+//? var => let, const
+// var x = 4;
+// console.log(window);
+
+// var user = {
+//   name: "Abbos",
+// };
+
+// user.age = 14;
+
+// var location = "Hello";
+
+// console.log(window);
+
+//* let vs const
+// var variableName = "value";
+// variableName = 2
+
+// let x = "value";
+// x = 5;
+// console.log(x);
+
+// const pi = 3.14;
+// pi = 3;
+// console.log(pi);
+
+//? confirm
+// const isApproved = confirm("Do you really want to delete it?");
+// console.log(isApproved);
