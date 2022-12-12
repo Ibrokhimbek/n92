26700dd5bc7c697e5ee15dbc70783c069ed2fb90
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-12 19:31:56 +0500

    array length

--- 3-month/Lesson 4/js/main.js
+++ 3-month/Lesson 4/js/main.js
@@ -1,7 +1,20 @@
 //? Data structure => Array
 
-// const array = ["Botir", "Sobir", "Qodir"];
-// console.log(array[2]);
+// const array = [
+//   { name: "Pistonchi" },
+//   ["Hello", "Salom", "Guten abend", "Gute nacht"],
+//   2912912,
+//   "Sobir",
+//   "Qodir",
+//   true,
+//   NaN,
+//   null,
+//   undefined,
+//   function () {
+//     console.log("Hello world");
+//   },
+// ];
+// console.log(array.length);
 
 // const user = {
 //   name: "Pistonchi",
