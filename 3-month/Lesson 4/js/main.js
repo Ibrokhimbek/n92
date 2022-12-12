dda8fa85e53f579eeeafb2737a80d290dcbfda37
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-12 19:44:00 +0500

    Adding new element to array

--- 3-month/Lesson 4/js/main.js
+++ 3-month/Lesson 4/js/main.js
@@ -23,3 +23,18 @@
 // console.log(user.friends[1]);
 // user.name;
 // user["name"];
+
+//* Arrayga yangi ma'lumot qo'shish
+// let arr = [1, 2, 3, 4];
+// arr = { name: "Palonchi" };
+// arr[9] = "Hello";
+// console.log(arr);
+// console.log(arr[5]);
+
+// let users = [];
+
+// users[users.length] = "Ibrohim";
+// users[users.length] = "Ismoil";
+// users[users.length] = "Abbos";
+
+// console.log(users);
