1724d64ee379edb727aed17f397ec52eeb22602b
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-12 19:24:13 +0500

    intro into array

---
+++ 3-month/Lesson 4/js/main.js
@@ -0,0 +1,12 @@
+//? Data structure => Array
+
+// const array = ["Botir", "Sobir", "Qodir"];
+// console.log(array[2]);
+
+// const user = {
+//   name: "Pistonchi",
+//   friends: ["Botir", "Sobir", "Qodir"],
+// };
+// console.log(user.friends[1]);
+// user.name;
+// user["name"];
