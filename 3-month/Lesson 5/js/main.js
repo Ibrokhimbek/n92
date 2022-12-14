5bb5090053afb522e0c203525f7c01b8f1369007
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-14 19:44:50 +0500

    Uyga vazifa

---
+++ 3-month/Lesson 5/js/main.js
@@ -0,0 +1,39 @@
+//? v 1.0.0 => Uyga vazifa
+// const signUpBtn = document.getElementById("btn__signUp");
+// const signInBtn = document.getElementById("btn__signIn");
+
+// const user = {
+//   name: null,
+//   password: null,
+// };
+
+//* 1. adding functions to the button
+// signUpBtn.onclick = function () {
+//   //* 2. asking questions from the user
+//   const username = prompt("Enter your username:");
+//   const password = prompt("Enter your password:");
+
+//   //* 3. adding data into the database
+//   user.name = username;
+//   user.password = password;
+
+//   //* 4. giving info to the user
+//   alert("Successfully sign up!!!");
+//   console.log(user);
+// };
+
+//* 1. adding functions to the button
+// signInBtn.onclick = function () {
+//   if (user.name && user.password) {
+//     //* 2. asking questions from the user
+//     const username = prompt("Enter your username:");
+//     const password = prompt("Enter your password:");
+//     if (user.name === username && user.password === password) {
+//       alert("Welcome to the website!");
+//     } else {
+//       alert("Your data is incorrect!");
+//     }
+//   } else {
+//     alert("Please, create new account!");
+//   }
+// };
