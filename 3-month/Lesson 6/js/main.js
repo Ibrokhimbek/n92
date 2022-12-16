777125c2ffb51e1312566200980b5e4142bfa6f8
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-16 19:38:00 +0500

    Array methods

---
+++ 3-month/Lesson 6/js/main.js
@@ -0,0 +1,27 @@
+//? Array methods
+//* concat()
+// const arr = ["Jasur", "Laylo", "Sevinch", "Userbek", "Mishkabek"];
+// const newArr = arr.concat(
+//   [1, 2, 3, 4, 5],
+//   ["Tal'at", "Gulchapchap"],
+//   "Ibrohim",
+//   { name: "Asqar" }
+// );
+// console.log(newArr);
+// console.log(arr.concat([1, 2, 3, 4, 5]));
+//* join()
+// const arr = ["Jasur", "Laylo", "Sevinch", "Userbek", "Mishkabek"];
+// const newStrArr = arr.join(" ");
+// console.log(newStrArr);
+//* slice()
+// const arr = ["Jasur", "Laylo", "Sevinch", "Userbek", "Mishkabek"];
+// const newArr = arr.slice(1, 3); ["Laylo", "Sevinch"]
+// const newArr2 = arr.slice(1); ["Laylo", "Sevinch", "Userbek", "Mishkabek"]
+// console.log(arr.slice(1, 2));
+//* indexOf()
+// const arr = ["Jasur", "Laylo", "Sevinch", "Userbek", "Laylo", "Mishkabek"];
+// console.log(arr.indexOf("Laylo"));
+//* includes()
+// const arr = ["Jasur", "Laylo", "Sevinch", "Userbek", "Laylo", "Mishkabek"];
+// console.log(arr.includes("Sevinch"));
+// console.log(arr.includes("sevinch"));
