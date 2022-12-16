e4dbfa343940d892709ff35a0ef3f117456ea827
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-16 19:55:46 +0500

    string methods

--- 3-month/Lesson 6/js/main.js
+++ 3-month/Lesson 6/js/main.js
@@ -25,3 +25,23 @@
 // const arr = ["Jasur", "Laylo", "Sevinch", "Userbek", "Laylo", "Mishkabek"];
 // console.log(arr.includes("Sevinch"));
 // console.log(arr.includes("sevinch"));
+
+//? String methods
+//* substring()
+// const str = "Ibrohim";
+// const newStr = str.substring(0, 3);
+// const newStr2 = str.substring(0);
+// console.log(newStr);
+//* search()
+// const str = "Qambar maktabdan katta besh olib keldi";
+// const isFound = str.search("katta");
+// console.log(str.substring(isFound));
+//* charAt()
+// const str = "Ibrohim";
+// console.log(str.charAt(0));
+//* match()
+// const str = "Ibrohim";
+// console.log(str.match("Ibrohim"));
+//* split()
+// const str = "Qambar-maktabdan-katta besh-olib-keldi";
+// console.log(str.split(" "));
