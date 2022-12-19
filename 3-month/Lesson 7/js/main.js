4b101b7f68fb74282a48df600b6085eab3e8d462
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-19 19:44:36 +0500

    early return

--- 3-month/Lesson 7/js/main.js
+++ 3-month/Lesson 7/js/main.js
@@ -1,30 +1,39 @@
 //? Form validation
-const errorEl = document.querySelector("#error");
-const name = document.querySelector("#name");
-const password = document.querySelector("#password");
-const form = document.querySelector("#form");
+// const errorEl = document.querySelector("#error");
+// const name = document.querySelector("#name");
+// const password = document.querySelector("#password");
+// const form = document.querySelector("#form");
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault();
 
-  const errors = [];
+//   const errors = [];
 
-  if (name.value.length > 20) {
-    errors.push("Name must be less than 20 characters");
-  }
+//   if (name.value.length > 20) {
+//     errors.push("Name must be less than 20 characters");
+//   }
 
-  if (password.value.length < 8) {
-    errors.push("Password must be more than 8 characters");
-  }
+//   if (password.value.length < 8) {
+//     errors.push("Password must be more than 8 characters");
+//   }
 
-  if (errors.length > 0) {
-    errorEl.textContent = errors.join(", ");
-  } else {
-    errorEl.textContent = "Successfully registered!";
-  }
-});
+//   if (errors.length > 0) {
+//     errorEl.textContent = errors.join(", ");
+//   } else {
+//     errorEl.textContent = "Successfully registered!";
+//   }
+// });
 
 //? early return
+// function sum(a, b) {
+//   if (typeof a !== "number" || typeof b !== "number") {
+//     return undefined;
+//   } else {
+//     return a + b;
+//   }
+// }
+
+// console.log(sum(6, 5));
 //? Creating elements
 //? setAttribute()
 //? removeAttribute()
