06a7da39b9ceb2f4e982a6b6e1114614437f91a2
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-21 19:56:49 +0500

    webkitSpeechRecognition

---
+++ 3-month/Lesson 8/js/main.js
@@ -0,0 +1,29 @@
+//? webkitSpeechRecognition
+// const btn = document.querySelector("#btn");
+
+// const recognition = new webkitSpeechRecognition();
+// recognition.lang = "uz-UZ";
+// recognition.continuous = false;
+
+// btn.addEventListener("click", () => {
+//   recognition.start();
+// });
+
+// const colors = {
+//   qizil: "red",
+//   sariq: "yellow",
+//   yashil: "green",
+// };
+
+// recognition.onresult = (event) => {
+//   document.body.style.background = colors[event.results[0][0].transcript]
+//     ? colors[event.results[0][0].transcript]
+//     : "white";
+// };
+
+//? for of  vs for in loop
+//? infinite loop
+//? RegExp(), ASCII - American Standard Code for Information Interchange
+//? Keydown
+//? Keyup
+//? Kuypress
