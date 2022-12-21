b1aa23ee998888f9bf97ae66e04fa36d8b6942d2
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-21 20:09:13 +0500

    for in vs for of

--- 3-month/Lesson 8/js/main.js
+++ 3-month/Lesson 8/js/main.js
@@ -22,6 +22,32 @@
 // };
 
 //? for of  vs for in loop
+// const arr = ["Abror", "Asror", "Axror", "Barbara"];
+// for (val of arr) {
+//   console.log(val);
+// }
+
+// const arr = [];
+
+// const user = {
+//   name: "Ibrohim",
+//   surname: "Turaboev",
+// };
+
+// for of not for objects because objects are not iterable
+// for (val of user) {
+//   console.log(val);
+// }
+
+// for (key in user) {
+//   const element = [];
+//   element.push(key);
+//   element.push(user[key]);
+//   arr.push(element);
+// }
+
+// console.log(arr);
+
 //? infinite loop
 //? RegExp(), ASCII - American Standard Code for Information Interchange
 //? Keydown
