a3fa33086e7b790d9718c2a3c8c0052440054529
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-23 20:55:22 +0500

    toggle todo vs delete todo

--- 3-month/Lesson 9/js/main.js
+++ 3-month/Lesson 9/js/main.js
@@ -26,8 +26,30 @@
 //? Todo
 const todoForm = document.querySelector("#todoForm");
 const cards = document.querySelector("#cards");
+const todoTogglers = document.querySelectorAll("#todoRadio");
 
-const todosArr = [];
+let todosArr = [
+  {
+    id: 1,
+    task: "Blabla",
+    isCompleted: false,
+    isEditing: false,
+  },
+  {
+    id: 2,
+    task: "Blabla",
+    isCompleted: false,
+    isEditing: false,
+  },
+  {
+    id: 3,
+    task: "Blabla",
+    isCompleted: false,
+    isEditing: false,
+  },
+];
+
+render();
 
 todoForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -49,28 +71,35 @@ todoForm.addEventListener("submit", (event) => {
 function render() {
   cards.innerHTML = "";
   for (let i = 0; i < todosArr.length; i++) {
+    const todo = todosArr[i];
     const template = `
-      <div class="col-md-3 p-3 bg-light rounded">
+      <div class="col-md-3 p-3 ${
+        todo.isCompleted ? "bg-success text-light" : "bg-light"
+      } rounded">
         <div class="form-check form-switch">
           <input
             class="form-check-input"
             type="checkbox"
             role="switch"
-            id="flexSwitchCheckChecked"
+            id="todoRadio"
+            onchange="toggleCompleted(${todo.id})"
+            ${todo.isCompleted ? "checked" : ""}
           />
-          <label class="form-check-label" for="flexSwitchCheckChecked"
-            >${todosArr[i].task}</label
+          <label class="form-check-label" for="todoRadio"
+            >${todo.task}</label
           >
           <div class="d-flex justify-content-end gap-3 mt-3">
             <button
               class="btn btn-warning"
               type="button"
               id="edit_btn"
-              data-id="${todosArr[i].id}"
+              data-id="${todo.id}"
             >
               <i class="fas fa-pen"></i>
             </button>
-            <button class="btn btn-danger" type="button" id="delete_btn" data-id="${todosArr[i].id}">
+            <button class="btn btn-danger" type="button" id="delete_btn" onclick="deleteTodo(${
+              todo.id
+            })">
               <i class="fas fa-trash"></i>
             </button>
           </div>
@@ -78,5 +107,30 @@ function render() {
       </div>`;
 
     cards.innerHTML = cards.innerHTML + template;
+  }
+}
+
+function toggleCompleted(id) {
+  for (let i = 0; i < todosArr.length; i++) {
+    const todo = todosArr[i];
+    if (todo.id === id) {
+      todo.isCompleted = !todo.isCompleted;
+    }
+  }
+  render();
+}
+
+function deleteTodo(id) {
+  const isAccepted = confirm("Do you really delete this todo?");
+  if (isAccepted) {
+    const newArr = [];
+    for (let i = 0; i < todosArr.length; i++) {
+      const todo = todosArr[i];
+      if (todo.id !== id) {
+        newArr.push(todo);
+      }
+    }
+    todosArr = newArr;
+    render();
   }
 }
