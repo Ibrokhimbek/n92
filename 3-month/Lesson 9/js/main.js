7f9e4f50eeb59ebecf2ac18a290f4593b7b1bc61
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-23 19:40:49 +0500

    dataset vs this keyword

---
+++ 3-month/Lesson 9/js/main.js
@@ -0,0 +1,24 @@
+//? dataset
+// const todos = document.querySelectorAll(".todo");
+// console.dir(todos[0].dataset.name);
+
+//? this
+// const user = {
+//   name: "Ibrohim",
+//   username: "Turaboev",
+//   age: 20,
+//   fullname: function () {
+//     console.log(this);
+//     return this.name + " " + this.username;
+//   },
+// };
+
+// console.log(user.fullname());
+
+//? this on elements
+// const todos = document.querySelectorAll(".todo");
+// for (let i = 0; i < todos.length; i++) {
+//   todos[i].addEventListener("click", function () {
+//     this.style.background = "red";
+//   });
+// }
