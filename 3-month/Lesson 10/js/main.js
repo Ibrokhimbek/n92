32107a9c60866babb348ffc8b640165f80fe0d8f
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-26 20:07:54 +0500

    callback function

---
+++ 3-month/Lesson 10/js/main.js
@@ -0,0 +1,26 @@
+//? callback functions
+// const btn = document.querySelector("#click");
+
+// btn.addEventListener("click", function () {
+//   console.log("Hello");
+// });
+
+// function clicker() {
+//   console.log("Hello world");
+// }
+// clicker()
+
+// function callbackHandler(firstname, birthyear, callback) {
+//   const currentYear = new Date().getFullYear();
+//   const template = `${firstname} bu yil ${currentYear - birthyear} yoshda!`;
+//   const returnedData = callback(template);
+//   console.log(returnedData);
+//   return firstname + " " + birthyear;
+// }
+
+// const data = callbackHandler("Abdujalil", 2004, function (mytemplate) {
+//   console.log(mytemplate);
+//   return "Hello world";
+// });
+
+// console.log(data);
