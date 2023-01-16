834f7d32ff26e1d767376649384ad3109f44231e
Ibrokhimbek <ibrokhim1348@gmail.com>
2023-01-16 20:05:34 +0500

    reduce

---
+++ 4-month/Lesson 3/js/main.js
@@ -0,0 +1,23 @@
+//? reduce
+// const array = [1, 2, 3, 4, 5];
+
+// const result = arr.reduce((prev, curr, idx, arr) => {
+//   return prev + curr;
+// }, 0);
+// console.log(result);
+
+// function myReducer(arr, cb, initVal) {
+//   let prev = initVal ? initVal : arr[0];
+
+//   for (let i = initVal ? 0 : 1; i < arr.length; i++) {
+//     prev = cb(prev, arr[i], i, arr);
+//   }
+
+//   return prev;
+// }
+
+// console.log(
+//   myReducer(array, (prevVal, currVal, index, arr) => {
+//     return prevVal + currVal;
+//   })
+// );
