61cffbcdef90a873dba723c2f0498e9d676dda38
Ibrokhimbek <ibrokhim1348@gmail.com>
2023-01-27 19:36:38 +0500

    Destructuring

---
+++ 4-month/Lesson 8/js/main.js
@@ -0,0 +1,26 @@
+//? Destructuring
+//* object
+// const user = {
+//   name: "Palonchi",
+//   age: 199,
+//   birthyear: 1822,
+// };
+
+// function myFunc({ name, birthyear }, num) {
+//   // console.log(obj.name);
+//   // const { name, birthyear } = obj;
+//   console.log(name, birthyear, num);
+// }
+
+// myFunc(user, 18);
+
+//* array
+// const users = ["Sardor", "Shaxzod", "Ozod", "Abdulla"];
+
+// const [user1, user2, user3] = users;
+
+// function bla([user1, user2, user3, user4]) {
+//   console.log(user4);
+// }
+// bla(users);
+// console.log(user1);
