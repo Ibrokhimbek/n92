6894b438b5d1aa4d08e9dccdb2d74a04f1a51a2b
Ibrokhimbek <ibrokhim1348@gmail.com>
2023-01-30 19:56:35 +0500

    Request types

---
+++ 4-month/Lesson 9/js/main.js
@@ -0,0 +1,52 @@
+//? GET, POST, DELETE, PUT
+//* GET
+// fetch("http://xolisnazar.uz/api/index/currency")
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
+
+//* POST
+// fetch("https://reqres.in/api/users", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     name: "Ibrohim",
+//     job: "Software Engineer",
+//   }),
+// })
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
+
+//* DELETE
+// fetch("https://reqres.in/api/users/3", {
+//   method: "DELETE",
+// }).then((res) => {
+//   console.log(res);
+// });
+
+//* PUT
+// fetch("https://reqres.in/api/users/2", {
+//   method: "PUT",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     name: "Ibrohim",
+//     job: "Software Engineer",
+//   }),
+// })
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
